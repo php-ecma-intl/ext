d958769160fc62fb2a9b57794a270218fd2613c9
Ben Ramsey <ben@benramsey.com>
2023-07-14 22:15:11 -0500

    test: remove no-op tests

--- tests/pest/intl402/Intl/supportedValuesOf/coerced-to-string.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// This test is a no-op test on purpose. It exists for the sake of parity
-// with the Test262 test suite.
-it('does not support coercion of input to string')
-    ->expect(true)
-    ->toBe(true);
