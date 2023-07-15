f40dc07c3c958407d0078f7eb4f2d382f5f72ddd
Ben Ramsey <ben@benramsey.com>
2023-07-15 00:35:37 -0500

    chore: add WebKit copyright notices to applicable source files

--- src/ecma402/language_tag.cpp
+++ src/ecma402/language_tag.cpp
@@ -7,6 +7,36 @@
    | this package in the file LICENSE, and is available at the following  |
    | web address: https://opensource.org/license/mit/                     |
    +----------------------------------------------------------------------+
+
+     Portions of this source file are derived from
+     WebKit Open Source Project and JavaScriptCore Project
+
+     Copyright (c) 2015 Andy VanWagoner (andy@vanwagoner.family)
+     Copyright (c) 2015 Sukolsak Sakshuwong (sukolsak@gmail.com)
+     Copyright (c) 2016-2021 Apple Inc. All rights reserved.
+     Copyright (c) 2020 Sony Interactive Entertainment Inc.
+
+     Redistribution and use in source and binary forms, with or without
+     modification, are permitted provided that the following conditions
+     are met:
+
+     1. Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+     2. Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+
+     THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+     AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+     THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+     PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+     BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+     CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+     SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+     INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+     CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+     ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+     THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include "language_tag.h"
