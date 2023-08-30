c3e877fa48dc1b8092be6651434c55439053a3be
Ben Ramsey <ben@benramsey.com>
2023-08-29 20:16:57 -0500

    chore: main is now for 0.1.4-dev

--- src/php/ecma_intl.h
+++ src/php/ecma_intl.h
@@ -18,7 +18,7 @@
 extern zend_module_entry ecma_intl_module_entry;
 #define phpext_ecma_intl_ptr &ecma_intl_module_entry
 
-#define PHP_ECMA_INTL_VERSION "0.1.3"
+#define PHP_ECMA_INTL_VERSION "0.1.4-dev"
 
 #if defined(ZTS) && defined(COMPILE_DL_ECMA_INTL)
 ZEND_TSRMLS_CACHE_EXTERN()
